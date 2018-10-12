3eaf27289442b64242cedf90f5834f5606a60069
Dylan Broussard <deraw@outlook.fr>
2018-10-12 16:21:40 +0200

    ♻️ Update LangBtn & add colors

--- src/components/LangBtn.vue
+++ src/components/LangBtn.vue
@@ -2,17 +2,52 @@
 	<v-menu
 		class="menu-depth"
 		allow-overflow
+		offset-y
 	>
 		<v-btn
 			slot="activator"
 			:aria-label="`${label} ${languages[currentLangUsed].nativeName}`"
-			flat
+
+			:absolute="absolute"
+			:active-class="activeClass"
+			:append="append"
+			:block="block"
+			:bottom="bottom"
+			:color="color"
+			:dark="dark"
+			:depressed="depressed"
+			:disabled="disabled"
+			:fab="fab"
+			:fixed="fixed"
+			:flat="flat"
+			:icon="icon"
+			:large="large"
+			:left="left"
+			:light="light"
+			:loading="loading"
+			:outline="outline"
+			:right="right"
+			:ripple="ripple"
+			:round="round"
+			:small="small"
+			:tag="tag"
+			:target="target"
+			:top="top"
+			:type="type"
+			:value="btnValue"
 		>
+			<img
+				v-if="flags"
+				:src="`${flagsUrl}${currentLangUsed}.svg`"
+				class="current-flag"
+				alt=""
+			>
 			<span
-				v-if="$vuetify.breakpoint.mdAndUp"
+				v-if="displayTextBtn"
 				class="ml-2"
 			>{{ languages[currentLangUsed].nativeName }}</span>
 			<SvgIcon
+				v-if="displayArrow"
 				size="10px"
 				class="ml-2"
 				icon="expand"
@@ -29,7 +64,15 @@
 					class="ma-0"
 					@click="changeLang(lang)"
 				>
-					<v-list-tile-title>{{ item.name }}</v-list-tile-title>
+					<v-list-tile-avatar v-if="flags">
+						<img
+							:src="`${flagsUrl}${lang}.svg`"
+							class="flag-img"
+							alt=""
+						>
+					</v-list-tile-avatar>
+
+					<v-list-tile-title>{{ item.nativeName }}</v-list-tile-title>
 				</v-btn>
 			</v-list-tile>
 		</v-list>
@@ -44,22 +87,147 @@
 	export default Vue.extend({
 		name: 'LangBtn',
 		props: {
+			// Shared
 			availableLanguages: {
 				type: [Object, Array, String],
 				default: '*'
-			},
-			callback: {
-				type: Function,
-				required: true
 			},
 			label: {
 				type: String,
 				default: 'Choix de la langue. Actuellement :'
 			},
-			currentLang: {
+			flags: {
+				type: Boolean,
+				default: false
+			},
+			flagsUrl: {
 				type: String,
-				default: 'fr'
+				default: '/img/flags/'
+			},
+			displayTextBtn: {
+				type: Boolean,
+				default: true
+			},
+			displayArrow: {
+				type: Boolean,
+				default: true
+			},
+			value: {
+				type: String,
+				default: undefined
+			},
+			// Btn
+			absolute: {
+				type: Boolean,
+				default: false
+			},
+			activeClass: {
+				type: String,
+				default: 'v-btn--active'
+			},
+			append: {
+				type: Boolean,
+				default: false
+			},
+			block: {
+				type: Boolean,
+				default: false
+			},
+			bottom: {
+				type: Boolean,
+				default: false
+			},
+			color: {
+				type: String,
+				default: '##eaeaea'
+			},
+			dark: {
+				type: Boolean,
+				default: false
+			},
+			depressed: {
+				type: Boolean,
+				default: true
+			},
+			disabled: {
+				type: Boolean,
+				default: false
+			},
+			fab: {
+				type: Boolean,
+				default: false
+			},
+			fixed: {
+				type: Boolean,
+				default: false
+			},
+			flat: {
+				type: Boolean,
+				default: false
+			},
+			icon: {
+				type: Boolean,
+				default: false
+			},
+			large: {
+				type: Boolean,
+				default: false
+			},
+			left: {
+				type: Boolean,
+				default: false
+			},
+			light: {
+				type: Boolean,
+				default: false
+			},
+			loading: {
+				type: Boolean,
+				default: false
+			},
+			outline: {
+				type: Boolean,
+				default: true
+			},
+			right: {
+				type: Boolean,
+				default: false
+			},
+			ripple: {
+				type: [Boolean, Object],
+				default: true
+			},
+			round: {
+				type: Boolean,
+				default: false
+			},
+			small: {
+				type: Boolean,
+				default: false
+			},
+			tag: {
+				type: String,
+				default: 'button'
+			},
+			target: {
+				type: String,
+				default: undefined
+			},
+			top: {
+				type: Boolean,
+				default: false
+			},
+			type: {
+				type: String,
+				default: 'button'
+			},
+			btnValue: {
+				default: undefined
 			}
+		},
+		model: {
+			prop: 'value',
+			event: 'change'
 		},
 		computed: {
 			languages(): object {
@@ -76,15 +244,18 @@
 				return data;
 			}
 		},
+		mounted() {
+			this.$emit('change', this.currentLangUsed);
+		},
 		data(this: any): any {
 			return {
 				languagesData: this.getLanguagesWithData(),
-				currentLangUsed: this.currentLang
+				currentLangUsed: this.value || 'fr'
 			};
 		},
 		methods: {
 			changeLang(lang: string): void {
-				this.callback(lang);
+				this.$emit('change', lang);
 				this.currentLangUsed = lang;
 			},
 			getLanguagesWithData(): object[] {
@@ -105,9 +276,9 @@
 		border-radius: 0%;
 	}
 
-	.v-menu .v-menu__content {
+	.v-menu__content {
 		overflow: auto;
-		max-height: 75vh;
+		max-height: 300px;
 	}
 
 	.v-list {
