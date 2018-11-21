b3b4eb6311e68b43567512a19c14220129e4ff36
Dylan Broussard <deraw@outlook.fr>
2018-11-21 17:48:05 +0100

    🔧 Update build config
    Disable chunk splitting  & minify output

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
+
 module.exports = {
 	css: {
 		extract: false
@@ -6,6 +9,21 @@ module.exports = {
 		entry: './playground/main.ts',
 		output: {
 			libraryExport: 'default'
-		}
+		},
+		optimization: {
+			minimize: true,
+			minimizer: [new UglifyJsPlugin({
+				include: /\.min\.js$/
+			})]
+		},
+		plugins: [
+			new webpack.optimize.LimitChunkCountPlugin({
+				maxChunks: 1
+			})
+		]
+	},
+	chainWebpack: config => {
+		config.optimization.delete('splitChunks');
+		config.optimization.splitChunks(false);
 	}
 };
