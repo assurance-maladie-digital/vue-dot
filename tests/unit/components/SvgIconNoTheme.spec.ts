b5a2cedae985b87e9befcb3794de11424e392d89
Dylan Broussard <deraw@outlook.fr>
2019-02-06 15:54:44 +0100

    ✅ tests: Add unit tests for SvgIcon

---
+++ tests/unit/components/SvgIconNoTheme.spec.ts
@@ -0,0 +1,31 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import SvgIcon from '@/components/SvgIcon.vue';
+
+import Vuetify from 'vuetify';
+import VueDot from '@/index';
+import { default as theme } from '../theme.json';
+
+const localVue = createLocalVue();
+localVue.use(Vuetify);
+
+localVue.use(VueDot);
+
+describe('SvgIcon.vue without theme', () => {
+	const build = () => {
+		const wrapper = shallowMount(SvgIcon, {
+			localVue,
+			propsData: {
+				icon: 'false'
+			}
+		});
+
+		return wrapper;
+	};
+
+	it('renders correctly', () => {
+		const wrapper = build();
+
+		expect((wrapper.vm as any).themeIcons).toBe({});
+		expect(wrapper.html()).toMatchSnapshot();
+	});
+});
