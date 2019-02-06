27bd7cfd3071a3095417ef2ee16bd2f9a2ae9b23
Dylan Broussard <deraw@outlook.fr>
2019-02-06 16:41:17 +0100

    ✅ tests: Add unit test for DatePicker

---
+++ tests/unit/components/DatePicker.spec.ts
@@ -0,0 +1,32 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import DatePicker from '@/components/DatePicker.vue';
+
+import Vuetify from 'vuetify';
+import VueDot from '@/index';
+import { default as theme } from '../theme.json';
+
+const localVue = createLocalVue();
+localVue.use(Vuetify);
+
+localVue.use(VueDot, {
+	theme
+});
+
+describe('DatePicker.vue', () => {
+	const build = () => {
+		const wrapper = shallowMount(DatePicker, {
+			localVue,
+			propsData: {
+				max: '2019-02-06'
+			}
+		});
+
+		return wrapper;
+	};
+
+	it('renders correctly', () => {
+		const wrapper = build();
+
+		expect(wrapper.html()).toMatchSnapshot();
+	});
+});
